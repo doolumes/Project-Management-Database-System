0585f7cc6f7e3cc0511526067fdf317b62f8a105
Mason7579 <yzhan308@cougarnet.uh.edu>
2023-03-20 03:00:41 -0500

    I did the outsource page but the html does not work, if someone can take a look that would be great

---
+++ MVC-Test/Controllers/OutsourceController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Group6Application.Models;
+using Group6Application.Model;
+
+namespace MVC_Test.Controllers
+{
+    public class OutsourceController : Controller
+    {
+        [Route("Outsource")]
+
+        public ActionResult Index()
+        {
+            string viewPath = "Views/Outsource/Index.cshtml";
+
+            OutsourceView viewModel = new OutsourceView()
+            {
+                Outsource = new List<OutsourceTemplate>()
+                {
+                    new OutsourceTemplate
+                    {
+                        Name = "John",
+                        Outsource_ID = 001,
+                        Email = "john@gmail.com",
+                        Phone_number = 8324560921,
+                        Job = "security",
+                        Task_ID = 003
+                    },
+                }
+            };
+            return View(viewPath, viewModel);
+        }
+    }
+}
---
+++ MVC-Test/Models/OutsourceModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Group6Application.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+
+namespace Group6Application.Model
+{
+    public class OutsourceTemplate
+    {
+        public string? Name { get; set; }
+        public int? Outsource_ID { get; set; }
+        public string? Email { get; set; }
+        public long? Phone_number { get; set; }
+        public string? Job { get; set; }
+        public int? Task_ID { get; set; }
+    }
+
+    public class OutsourceView
+    {
+        public List<OutsourceTemplate> Outsource = new List<OutsourceTemplate>();
+    }
+}
