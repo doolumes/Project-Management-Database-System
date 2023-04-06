c563a2b22203dbcadf75b5426da1e69985b3e9f7
Mason7579 <yzhan308@cougarnet.uh.edu>
2023-04-06 03:07:46 -0500

    this is what I have for the login page

---
+++ MVC-Test/Controllers/LoginController.cs
@@ -0,0 +1,55 @@
+﻿using Group6Application.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MVC_Test.Models;
+using Npgsql;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Net;
+using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
+
+namespace Group6Application.Controllers
+{
+    public class LoginController : Controller
+    {
+        SqlConnection con = new SqlConnection();
+        SqlCommand com = new SqlCommand();
+        SqlDataReader dr;
+        [Route("Login")]
+        public ActionResult Login()
+        {
+            string viewPath = "Views/Login/login.cshtml";
+            return View(viewPath);
+        }
+
+        void connectionString()
+        {
+            con.ConnectionString = "Server=20.124.84.12;Port=5432;Database=Group6-PMS;User Id=postgres;Password=KHf37p@&R2hf2l";
+        }
+		[Route("Verify")]
+		public ActionResult Verify(Account acc)
+        {
+			string viewPath = "Views/Employee/Index.cshtml";
+			string viewPath2 = "Views/Login/login.cshtml";
+			connectionString();
+            con.Open();
+            com.Connection = con;
+            com.CommandText = "SELECT * FROM \"Authentication\" WHERE \"Username\"='"+acc.Username+"' AND \"Password\"='"+acc.Password+"' ";
+            dr = com.ExecuteReader();
+            if(dr.Read())
+            {
+				con.Close();
+                return View(viewPath);
+            }
+            else
+            {
+				con.Close();
+                return View(viewPath2);
+            }
+        }
+    }
+}
---
+++ MVC-Test/Models/Login.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Group6Application.Model;
+
+namespace Group6Application.Models
+{
+    public class Account
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
