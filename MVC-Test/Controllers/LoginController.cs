9616d8d9090fa1dea5b09e52df0aa5ca6da93d1b
Mason7579 <yzhan308@cougarnet.uh.edu>
2023-04-07 02:05:51 -0500

    register page

--- MVC-Test/Controllers/LoginController.cs
+++ MVC-Test/Controllers/LoginController.cs
@@ -20,18 +20,18 @@ using System.Reflection.Metadata;
 
 namespace Group6Application.Controllers
 {
-    public class LoginController : Controller
-    {
+	public class LoginController : Controller
+	{
 		private static string _connectionString = "Server=20.124.84.12;Port=5432;Database=Group6-PMS;User Id=postgres;Password=KHf37p@&R2hf2l";
-        [Route("Login")]
-        public ActionResult Login()
-        {
-            string viewPath = "Views/Login/login.cshtml";
-            return View(viewPath);
-        }
+		[Route("Login")]
+		public ActionResult Login()
+		{
+			string viewPath = "Views/Login/login.cshtml";
+			return View(viewPath);
+		}
 		[Route("Verify")]
 		public ActionResult Verify(string Username, string Password)
-        {
+		{
 			string username = Username;
 			string password = Password;
 			DataTable datatable = Data.login(username, password);
@@ -46,7 +46,51 @@ namespace Group6Application.Controllers
 				return RedirectToAction("Index", "Employee");
 			}
 		}
-    }
+
+		[Route("Login/Register")]
+		public ActionResult Register()
+		{
+			string viewPath = "Views/Login/register.cshtml";
+			return View(viewPath);
+		}
+		public ActionResult Register_DB(string Username, string Password)
+		{
+			bool submissionResult = false;
+			string errorMessage = "";
+			string sqlQuery = $"INSERT INTO \"Authentication\"(\"Username\",\"Password\") VALUES (@Username,@Password);";
+			using (NpgsqlConnection conn = new NpgsqlConnection(_connectionString))
+			{
+				conn.Open();
+				NpgsqlCommand command = new NpgsqlCommand("", conn);
+				NpgsqlTransaction sqlTransaction;
+				sqlTransaction = conn.BeginTransaction();
+				command.Transaction = sqlTransaction;
+
+				command.CommandText = sqlQuery.ToString();
+				command.Parameters.Clear();
+
+				command.Parameters.AddWithValue("@Username", Username);
+				command.Parameters.AddWithValue("@Password", Password);
+				command.ExecuteNonQuery();
+				sqlTransaction.Commit();
+				submissionResult = true;
+
+				try { }
+				catch (Exception e)
+				{
+					// error catch here
+					sqlTransaction.Rollback();
+					errorMessage = "We experienced an error while adding to database";
+				}
+				finally
+				{
+					conn.Close();
+				}
+			};
+
+			return Json(new { submissionResult = submissionResult, message = errorMessage });
+		}
+	}
 }
 
 
